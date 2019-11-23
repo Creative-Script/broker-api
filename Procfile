42c465bcdc7864d74f7f0ac91fbcb95f05d18def
James Mudidi <mudidi.jimmy@gmail.com>
2019-11-23 23:16:34 +0300

    refactor the app structure

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: python manage.py migrate
+web: gunicorn broker.wsgi --log-file -
--- broker/wsgi.py
+++ broker/wsgi.py
@@ -1,16 +1,7 @@
-"""
-WSGI config for broker project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'broker.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'broker.apps.settings')
 
 application = get_wsgi_application()
